03f31ab8b29a75d17c138235492904dd318622f0
devinekadeni <devinekadeni@gmail.com>
2017-12-13 18:11:44 +0700

    updated all

---
+++ auth/src/components/common/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+const Button = ({ btnOnPress, children }) => (
+        <TouchableOpacity onPress={btnOnPress} style={style.buttonStyle} >
+            <Text>{children}</Text>
+        </TouchableOpacity> 
+    ); 
+
+const style = {
+    buttonStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#F3f3f3'
+    }
+}
+
+export { Button };
